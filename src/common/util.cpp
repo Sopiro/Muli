836e40560fbbfb99776c026c23aabcda6846ad54
sopiro <sopiro.ldh@gmail.com>
2023-01-12 07:44:49 +0900

    Optimize convex hull code

--- src/common/util.cpp
+++ src/common/util.cpp
@@ -8,7 +8,7 @@ void ComputeConvexHull(const Vec2* vertices, int32 vertexCount, Vec2* outVertice
 {
     if (vertexCount < 3)
     {
-        memcpy(outVertices, vertices, vertexCount);
+        memcpy(outVertices, vertices, vertexCount * sizeof(Vec2));
         *outVertexCount = vertexCount;
         return;
     }
@@ -24,26 +24,27 @@ void ComputeConvexHull(const Vec2* vertices, int32 vertexCount, Vec2* outVertice
     }
     Vec2 bottom = vertices[index];
 
+    Vec2* sorted = new Vec2[vertexCount];
+    memcpy(sorted, vertices, vertexCount * sizeof(Vec2));
+    std::swap(sorted[index], sorted[0]);
+
     // Sort the vertices based on the angle related to the lowest vertex.
-    Vec2* sorted = (Vec2*)malloc(vertexCount * sizeof(Vec2));
-    std::partial_sort_copy(vertices, vertices + vertexCount, sorted, sorted + vertexCount,
-                           [&](const Vec2& a, const Vec2& b) -> bool {
-                               Vec2 ra = a - bottom;
-                               Vec2 rb = b - bottom;
+    std::sort(sorted + 1, sorted + vertexCount, [&](const Vec2& a, const Vec2& b) -> bool {
+        Vec2 ra = a - bottom;
+        Vec2 rb = b - bottom;
 
-                               float angleA = Atan2(ra.y, ra.x);
-                               float angleB = Atan2(rb.y, rb.x);
+        float angleA = Atan2(ra.y, ra.x);
+        float angleB = Atan2(rb.y, rb.x);
 
-                               if (angleA == angleB)
-                               {
-                                   return Dist2(bottom, a) < Dist2(bottom, b);
-                               }
-                               else
-                               {
-                                   return angleA < angleB;
-                               }
-                           });
-    muliAssert(sorted[0] == bottom);
+        if (angleA == angleB)
+        {
+            return Dist2(bottom, a) < Dist2(bottom, b);
+        }
+        else
+        {
+            return angleA < angleB;
+        }
+    });
 
     // Discard overlapped bottom vertices
     int32 i = 0;
@@ -62,7 +63,8 @@ void ComputeConvexHull(const Vec2* vertices, int32 vertexCount, Vec2* outVertice
         }
     }
 
-    int32 sp = 0; // stack pointer
+    // Initialize stack
+    int32 sp = 0;
     *outVertexCount = vertexCount - i;
     outVertices[sp++] = sorted[i++];
     outVertices[sp++] = sorted[i++];
@@ -98,7 +100,7 @@ void ComputeConvexHull(const Vec2* vertices, int32 vertexCount, Vec2* outVertice
         }
     }
 
-    free(sorted);
+    delete[] sorted;
 }
 
 std::vector<Vec2> ComputeConvexHull(const std::vector<Vec2>& vertices)
@@ -116,28 +118,27 @@ std::vector<Vec2> ComputeConvexHull(const std::vector<Vec2>& vertices)
             index = i;
         }
     }
-
     Vec2 bottom = vertices[index];
-    std::vector<Vec2> sorted(vertices.size());
 
-    std::partial_sort_copy(vertices.begin(), vertices.end(), sorted.begin(), sorted.end(),
-                           [&](const Vec2& a, const Vec2& b) -> bool {
-                               Vec2 ra = a - bottom;
-                               Vec2 rb = b - bottom;
+    std::vector<Vec2> sorted = vertices;
+    std::swap(sorted[index], sorted[0]);
 
-                               float angleA = Atan2(ra.y, ra.x);
-                               float angleB = Atan2(rb.y, rb.x);
+    std::sort(sorted.begin() + 1, sorted.end(), [&](const Vec2& a, const Vec2& b) -> bool {
+        Vec2 ra = a - bottom;
+        Vec2 rb = b - bottom;
 
-                               if (angleA == angleB)
-                               {
-                                   return Dist2(bottom, a) < Dist2(bottom, b);
-                               }
-                               else
-                               {
-                                   return angleA < angleB;
-                               }
-                           });
-    muliAssert(sorted[0] == bottom);
+        float angleA = Atan2(ra.y, ra.x);
+        float angleB = Atan2(rb.y, rb.x);
+
+        if (angleA == angleB)
+        {
+            return Dist2(bottom, a) < Dist2(bottom, b);
+        }
+        else
+        {
+            return angleA < angleB;
+        }
+    });
 
     // Discard overlapped bottom vertices
     size_t i = 0;
