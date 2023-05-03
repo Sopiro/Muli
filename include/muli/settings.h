7782e46ac06ab6db04641c6ff4e719f803d87672
sopiro <sopiro.ldh@gmail.com>
2023-05-03 19:45:24 +0900

    Tweak simulation parameters

--- include/muli/settings.h
+++ include/muli/settings.h
@@ -11,13 +11,13 @@ constexpr int32 max_local_polygon_vertices = 8;
 constexpr float linear_slop = 0.002f; // meters
 constexpr float position_solver_threshold = linear_slop * 3.0f;
 constexpr float toi_position_solver_threshold = linear_slop * 2.0f - linear_slop * 0.05f;
-constexpr float position_correction = 0.2f; // The baumgarte term (0.0 ~ 1.0)
+constexpr float position_correction = 0.2f;     // The baumgarte term (0.0 ~ 1.0)
 constexpr float toi_position_correction = 0.3f;
-constexpr float max_position_correction = 0.2f; // meters
+constexpr float max_position_correction = 0.1f; // meters
 
 // Collision detection settings
 constexpr int32 gjk_max_iteration = 20;
-constexpr float gjk_tolerance = epsilon * epsilon;
+constexpr float gjk_tolerance = epsilon;
 constexpr int32 epa_max_iteration = 20;
 constexpr float epa_tolerance = epsilon;
 constexpr float contact_merge_threshold = linear_slop * 0.001f;
