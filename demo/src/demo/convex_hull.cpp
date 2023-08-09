999d5ffc5cb95177d6366be384da7fa53b0d6dbc
sopiro <sopiro.ldh@gmail.com>
2023-08-09 12:12:49 +0900

    Remove warning: -Wformat

--- demo/src/demo/convex_hull.cpp
+++ demo/src/demo/convex_hull.cpp
@@ -70,7 +70,7 @@ public:
         ImGui::Begin("Convex hull", NULL,
                      ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_AlwaysAutoResize |
                          ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoBackground);
-        ImGui::TextColored(ImColor{ 12, 11, 14 }, "Removed vertices: %lld", vertices.size() - convexHull.size());
+        ImGui::TextColored(ImColor{ 12, 11, 14 }, "Removed vertices: %zu", vertices.size() - convexHull.size());
         ImGui::End();
     }
 
