4557c68195f3728cd4410e0366e54e3608431745
sopiro <sopiro.ldh@gmail.com>
2023-12-10 21:09:34 +0900

    Organize code

--- include/muli/primitives.h
+++ include/muli/primitives.h
@@ -5,37 +5,37 @@
 namespace muli
 {
 
+// Geometric primitives
+
 struct Point
 {
     Vec2 p;   // vertex position
-    int32 id; // vertex index
+    int32 id; // vertex index(id)
 };
 
 struct Edge
 {
-    Edge(const Point& p1, const Point& p2);
-    Edge(const Vec2& p1, const Vec2& p2, int32 id1 = -1, int32 id2 = -1);
+    Edge(const Point& point1, const Point& point2);
+    Edge(const Vec2& point1, const Vec2& point2, int32 id1 = -1, int32 id2 = -1);
 
     void ComputeProperty();
     void Translate(const Vec2& d);
     float GetLength2() const;
 
     Point p1, p2;
-
-    Vec2 tangent;
-    Vec2 normal;
+    Vec2 tangent, normal;
 };
 
-inline Edge::Edge(const Point& _p1, const Point& _p2)
-    : p1{ _p1 }
-    , p2{ _p2 }
+inline Edge::Edge(const Point& point1, const Point& point2)
+    : p1{ point1 }
+    , p2{ point2 }
 {
     ComputeProperty();
 }
 
-inline Edge::Edge(const Vec2& _p1, const Vec2& _p2, int32 _id1, int32 _id2)
-    : p1{ _p1, _id1 }
-    , p2{ _p2, _id2 }
+inline Edge::Edge(const Vec2& point1, const Vec2& point2, int32 id1, int32 id2)
+    : p1{ point1, id1 }
+    , p2{ point2, id2 }
 {
     ComputeProperty();
 }
