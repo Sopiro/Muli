043181d3e7b07fbeb35ad09a426722284d73bded
sopiro <sopiro.ldh@gmail.com>
2024-01-22 17:57:31 +0900

    Fix missing initial overlap condition

--- src/collision/distance.cpp
+++ src/collision/distance.cpp
@@ -72,23 +72,22 @@ bool ShapeCast(const Shape* a,
     output->t = 1.0f;
 
     float t = 0.0f;
-    Vec2 n{ 0.0f };
-    float r2 = a->GetRadius() + b->GetRadius();
-    Vec2 r = translationB - translationA;
+    Vec2 n = zero_vec2;
+
+    const float radii = a->GetRadius() + b->GetRadius();
+    const Vec2 r = translationB - translationA; // Ray vector
 
     Simplex simplex;
 
-    // SupportPoint support = CSOSupport(a, tfA, b, tfB, -r);
-
-    // Get CSO support point in -r direction
+    // Get CSO support point in inverse ray direction
     int32 idA = a->GetSupport(MulT(tfA.rotation, -r));
     Vec2 pointA = Mul(tfA, a->GetVertex(idA));
     int32 idB = b->GetSupport(MulT(tfB.rotation, r));
     Vec2 pointB = Mul(tfB, b->GetVertex(idB));
     Vec2 v = pointA - pointB;
 
-    float target = Max(default_radius, r2 - toi_position_solver_threshold);
-    float tolerance = linear_slop * 0.1f;
+    const float target = Max(default_radius, radii - toi_position_solver_threshold);
+    const float tolerance = linear_slop * 0.1f;
 
     const int32 maxIterations = 20;
     int32 iteration = 0;
@@ -97,19 +96,21 @@ bool ShapeCast(const Shape* a,
     {
         muliAssert(simplex.count < 3);
 
-        // Get CSO support point in -v direction
+        // Get CSO support point in search direction(-v)
         idA = a->GetSupport(MulT(tfA.rotation, -v));
         pointA = Mul(tfA, a->GetVertex(idA));
         idB = b->GetSupport(MulT(tfB.rotation, v));
         pointB = Mul(tfB, b->GetVertex(idB));
-        Vec2 p = pointA - pointB;
+        Vec2 p = pointA - pointB; // Outer vertex of CSO
 
-        // -v is the normal at hit point
+        // -v is the plane normal at p
         v.Normalize();
 
         // Find intersection with support plane
         float vp = Dot(v, p);
         float vr = Dot(v, r);
+
+        // March ray by (vp - target) / vr if the new t is greater
         if (vp - target > t * vr)
         {
             if (vr <= 0.0f)
@@ -131,7 +132,7 @@ bool ShapeCast(const Shape* a,
         vertex->pointA.id = idA;
         vertex->pointA.p = pointA;
         vertex->pointB.id = idB;
-        vertex->pointB.p = pointB + t * r; // This effectively shifts the ray origin to the new clip point
+        vertex->pointB.p = pointB + t * r; // This effectively shifts the ray origin to the new clip plane
         vertex->point = vertex->pointA.p - vertex->pointB.p;
         vertex->weight = 1.0f;
         simplex.count += 1;
@@ -140,16 +141,17 @@ bool ShapeCast(const Shape* a,
 
         if (simplex.count == 3)
         {
-            // Overlap
+            // Initial overlap
             return false;
         }
 
+        // Update search direciton
         v = simplex.GetClosestPoint();
 
         ++iteration;
     }
 
-    if (iteration == 0)
+    if (iteration == 0 || t == 0.0f)
     {
         // Initial overlap
         return false;
