44955375d48e4340c05429f051d4d285f64591bb
sopiro <sopiro.ldh@gmail.com>
2024-05-09 04:43:17 +0900

    Add linear allocator

---
+++ include/muli/linear_allocator.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "allocator.h"
+
+namespace muli
+{
+
+// You muse nest allocate/free pairs
+class LinearAllocator : public Allocator
+{
+public:
+    LinearAllocator(int32 initialCapacity = 10 * 1024);
+    ~LinearAllocator();
+
+    virtual void* Allocate(int32 size) override;
+    virtual void Free(void* p, int32 size) override;
+    virtual void Clear() override;
+
+    bool GrowMemory();
+
+    int32 GetCapacity() const;
+    int32 GetAllocation() const;
+    int32 GetMaxAllocation() const;
+
+private:
+    struct MemoryEntry
+    {
+        char* data;
+        int32 size;
+        bool mallocUsed;
+    };
+
+    MemoryEntry* entries;
+    int32 entryCount;
+    int32 entryCapacity;
+
+    char* mem;
+    int32 capacity;
+    int32 index;
+
+    int32 allocation;
+    int32 maxAllocation;
+};
+
+inline int32 LinearAllocator::GetCapacity() const
+{
+    return capacity;
+}
+
+inline int32 LinearAllocator::GetAllocation() const
+{
+    return allocation;
+}
+
+inline int32 LinearAllocator::GetMaxAllocation() const
+{
+    return maxAllocation;
+}
+
+} // namespace muli
---
+++ src/common/linear_allocator.cpp
@@ -0,0 +1,116 @@
+#include "muli/linear_allocator.h"
+
+namespace muli
+{
+
+LinearAllocator::LinearAllocator(int32 initialCapacity)
+    : entryCount{ 0 }
+    , entryCapacity{ 32 }
+    , index{ 0 }
+    , capacity{ initialCapacity }
+    , allocation{ 0 }
+    , maxAllocation{ 0 }
+{
+    mem = (char*)malloc(capacity);
+    memset(mem, 0, capacity);
+    entries = (MemoryEntry*)malloc(entryCapacity * sizeof(MemoryEntry));
+}
+
+LinearAllocator::~LinearAllocator()
+{
+    assert(index == 0 && entryCount == 0);
+
+    free(entries);
+    free(mem);
+}
+
+void* LinearAllocator::Allocate(int32 size)
+{
+    if (entryCount == entryCapacity)
+    {
+        // Grow entry array by half
+        MemoryEntry* old = entries;
+        entryCapacity += entryCapacity / 2;
+        entries = (MemoryEntry*)malloc(entryCapacity * sizeof(MemoryEntry));
+        memcpy(entries, old, entryCount * sizeof(MemoryEntry));
+        memset(entries + entryCount, 0, (entryCapacity - entryCount) * sizeof(MemoryEntry));
+        free(old);
+    }
+
+    MemoryEntry* entry = entries + entryCount;
+    entry->size = size;
+
+    if (index + size > capacity)
+    {
+        entry->data = (char*)malloc(size);
+        entry->mallocUsed = true;
+    }
+    else
+    {
+        entry->data = mem + index;
+        entry->mallocUsed = false;
+        index += size;
+    }
+
+    allocation += size;
+    if (allocation > maxAllocation)
+    {
+        maxAllocation = allocation;
+    }
+
+    ++entryCount;
+
+    return entry->data;
+}
+
+void LinearAllocator::Free(void* p, int32 size)
+{
+    assert(entryCount > 0);
+
+    MemoryEntry* entry = entries + (entryCount - 1);
+    assert(entry->data == p);
+    assert(entry->size == size);
+
+    if (entry->mallocUsed)
+    {
+        free(p);
+    }
+    else
+    {
+        index -= entry->size;
+    }
+
+    allocation -= entry->size;
+    --entryCount;
+
+    p = nullptr;
+}
+
+bool LinearAllocator::GrowMemory()
+{
+    assert(index == 0);
+
+    if (maxAllocation < capacity)
+    {
+        return false;
+    }
+
+    // Grow memory by half
+    free(mem);
+    capacity += capacity / 2;
+    mem = (char*)malloc(capacity);
+    memset(mem, 0, capacity);
+
+    return true;
+}
+
+void LinearAllocator::Clear()
+{
+    index = 0;
+    entryCount = 0;
+    entryCapacity = 0;
+    allocation = 0;
+    maxAllocation = 0;
+}
+
+} // namespace muli
